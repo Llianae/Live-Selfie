8616c147c641b35f60eeac4eaeb0c931b3723d39
ITSF-PCP-054\laureline.zittel <laureline.zittel@its-future.com>
2025-04-02 10:06:04 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {VideoCaptureComponent} from "./video-capture/video-capture.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [VideoCaptureComponent],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
